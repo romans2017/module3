27b607624f28d79b633811b2bc69690e39434606
romans2017 <savchuk.r0@gmail.com>
2021-10-19 18:23:17 +0300

    Use Common table expressions instead of temporary table in 2.sql

--- 2.sql
+++ 2.sql
@@ -1,28 +1,16 @@
 
-CREATE TEMP TABLE IF NOT EXISTS tt_projects_salaries
-(
-	id integer,
-	name varchar(100),
-	sum_salary integer
-) ON COMMIT DROP
-;
-
-TRUNCATE tt_projects_salaries
-;
-
-INSERT INTO tt_projects_salaries
-SELECT
-	projects.id,
-	projects.name,
-	SUM(developers.salary) AS sum_salary 
-FROM developers_projects
-JOIN developers ON developers_projects.developer_id = developers.id
-JOIN projects ON developers_projects.project_id = projects.id
-GROUP BY 
-	projects.id,
-	projects.name
-;
-
+WITH tt_projects_salaries AS (
+	SELECT
+		projects.id,
+		projects.name,
+		SUM(developers.salary) AS sum_salary 
+	FROM developers_projects
+	JOIN developers ON developers_projects.developer_id = developers.id
+	JOIN projects ON developers_projects.project_id = projects.id
+	GROUP BY 
+		projects.id,
+		projects.name
+)
 SELECT
 	tt_projects_salaries.id,
 	tt_projects_salaries.name,
